c915053fa73f7ab1395e47d0d538f1ea85d8c26c
TheGentleGiant <45845625+TheGentleGiant@users.noreply.github.com>
2020-04-07 17:04:28 +0200

    Removed Old Files

--- TheAwesomeTinyEngine/Code/main.cpp
+++ TheAwesomeTinyEngine/Code/main.cpp
@@ -1,12 +1,7 @@
 #include <SDL.h>
 #include <iostream>
 #include "Engine/Engine.h"
-#include "GameObject.h"
-#include "GameWorld.h"
-#include "Player.h"
-#include "Enemy.h"
 
-GameObject *_player;
 SDL_Renderer* _renderer;
 
 int main(int argc, char* argv[])
@@ -24,18 +19,13 @@ int main(int argc, char* argv[])
 	//SDL_Event e;
 	while (IsEngineOpen())
 	{
-		GameWorld _game;
-		_game.SpawnGameObject(new Player(Vector(10.f, 10.f), "Res/Ma_Man.png"));
 
-		_game.SpawnGameObject(new Enemy(Vector( 300.f, 300.f), 10.f, "Res/Ma_Man_Enemy.png" ));
 
 		while (IsEngineOpen())
 		{
 			ClearEngine();
 			frameStart = SDL_GetTicks();
 			/***GAME LOOP***/
-			_game.Update();
-			_game.Render();
 
 			/*FRAME STABILIZER*/
 			frameTime = SDL_GetTicks() - frameStart;
